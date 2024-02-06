5a8e3ab3b43a5377f0e3f3a96fe06096936f4b6f
iamdaniel14 <96418140+iamdaniel14@users.noreply.github.com>
2024-02-06 20:51:26 +0100

    third commit

--- particles.js
+++ particles.js
@@ -1,10 +1,12 @@
 class Particles{
  constructor(x,y,m) {
 this.pos =createVector (x,y);
-this.vel=createVector (0,0)
+this.vel=p5.Vector.random2D()
 this.acc=createVector(0,0);
 this.mass =m;
 this.r=sqrt(this.mass)*10;
+this.col='red';
+this.col1='white';
 
     }
 
@@ -13,32 +15,53 @@ let f=p5.Vector.div(force,this.mass);
 this.acc.add(f);
     }
 
-show (){
-push ()
-fill('white');
+show (i){
+push ();
+noStroke();
+
+if (i%2==1){ 
+   fill (this.col1)
+} else {
+    fill (this.col)
+}
 circle (this.pos.x, this.pos.y,this.r*2);
-pop () 
+pop () ;
 
 }
 
-gravity(attractor){
+gravity(attractor,i){
 let force =p5.Vector.sub(attractor.pos,this.pos);
+let d =force.mag();
 let dSqr=force*force;
 dSqr=constrain(dSqr,0,200);
 force.normalize();
-let gravity=1;
+let gravity=0.00000069;
+
+
+if(d<100 )  {
 let strength=gravity*(this.mass*attractor.mass)/dSqr;
-force.setMag(strength);
+force.setMag(strength).mult(-1);
 this.applyForce(force);
+} else {
 
-
+}
 }
 
 update () {
 this.vel.add(this.acc);
 this.vel.limit (1);
 this.pos.add (this.vel);
-this.acc.set(0)
+this.acc.set(0);
+this.edges();
 }
+edges (){
+if(this.pos.x>width-this.r ||this.pos.x<this.r){
+    this.vel.x*=-1;
+} else if
+(this.pos.y>height-this.r ||this.pos.y<this.r){
+this.vel.y*=-1;
+    }
 
 }
+}
+
--- sketch.js
+++ sketch.js
@@ -5,34 +5,23 @@ let attractors =[];
 function setup () {
  createCanvas(windowWidth,windowHeight);
  
-
-
-
- for (let i=0; i<20; i++){
+ for (let i=0; i<100; i++){
  let x=random(width);
  let y=random(width);
- let mass=random(1,9);
+ let mass=random(1,2);
  let p=new Particles(x,y,mass);
  particles.push(p);
  }
 }
 
 function draw () {
-    background(0)
-
-    
-   
-
-
-
-    for (let p of particles){
-        
-     p.update();
-     p.show();
-    for (let b of particles) { 
-        if (p !==b ) {
-
-            p.gravity(b)
+background(0)
+   for (let i=0;i<particles.length;i++) { 
+    particles[i].update();
+    particles[i].show(i);
+    for (let j=0;j<particles.length;j++) { 
+    if ( particles[i] !== particles[j] ) {
+ particles[i].gravity( particles[j],i)
         }
     
    
